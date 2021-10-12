b8f5a51feb27cc5e51b6a8d34f5baa2329aedc3f
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2021-10-12 15:09:52 +0530

    components added

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,10 +1,19 @@
+<script context="module">
+  export async function preload(page) {
+    console.log(page);
+    return { state: page.params["state"] };
+  }
+</script>
+
 <script>
+  export let state;
+
   import CovidData from "../components/CovidData.svelte";
   import CovidGraph from "../components/CovidGraph.svelte";
 
   import TableContainer from "../components/TableContainer.svelte";
 </script>
 
+<h1>{state}</h1>
 <CovidData />
 <CovidGraph />
-<TableContainer />
