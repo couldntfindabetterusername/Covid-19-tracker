54134d2a60d8e4cd0e23f11d422d8a428dd828ba
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2021-10-12 15:46:47 +0530

    navbar completed

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,35 @@
 <script>
   export let segment;
+
+  let isOpen = false;
+  function toggleNav() {
+    isOpen = !isOpen;
+  }
 </script>
+
+<nav class="navbar" role="navigation" aria-label="main navigation ">
+  <div class="navbar-brand">
+    <a href="/" class="navbar-item"> <img src="favicon.png" alt="virus" /></a>
+    <span
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false"
+      on:click={toggleNav}
+      class:is-active={isOpen}
+    />
+
+    <span aria-hidden="true" />
+    <span aria-hidden="true" />
+    <span aria-hidden="true" />
+  </div>
+  <div class="navbar-menu" class:is-active={isOpen}>
+    <div class="navbar-start">
+      <a href="/" class="navbar-item" class:is-active={segment === undefined}
+        >Home</a
+      >
+      <a href="/about" class="navbar-item" class:is-active={segment === "about"}
+        >About</a
+      >
+    </div>
+  </div>
+</nav>
